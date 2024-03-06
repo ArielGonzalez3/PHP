90986e030ddc04d75582255501eeacd25c70b319
ArielGonzalez3 <elvisarielgonzalez@gmail.com>
2024-03-06 11:58:37 +0100

    Damos Algunos estilos a la web de estudio de PHP

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Mi Pagina Web</title>
 	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
 </head>
 <body>
-<h1>Estudiando PHP</h1>
+<h1 class="title">Estudiando PHP</h1>
 
 <section>
 	<div>
